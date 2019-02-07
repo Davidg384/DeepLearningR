73a8a5e44c25c44ffe76fa2c50e568a4d58cf16d
David G <davidg384@gmail.com>
2019-02-07 17:25:23 -0500

    added useful comments

--- Homework 3/Homework_3.R
+++ Homework 3/Homework_3.R
@@ -5,32 +5,34 @@ library(DataExplorer)
 library(tidyverse)
 library (recipes)
 
+# Imports raw data
 Mydata_raw <- read.csv(file="E:/DeepLearningDataSets/supersym.csv", header=TRUE, sep=",")
 #                            ^ ^ ^ link to supersym.csv
 
+# view raw data
 glimpse(Mydata[,1])
 
+
+# Organizes data 
 Mydata <- Mydata_raw %>%
   select(target, everything())
 
-#plot_missing(Mydata_raw)
-
-#(Train_1)
-
+# define recipe object to center, scale, and prep data
 rec_obj <- recipe(target ~ ., data = Mydata) %>%
   step_center(all_numeric(), -all_outcomes()) %>%
   step_scale(all_numeric(), -all_outcomes()) %>%
   prep(data = Mydata)
 
-#rec_obj
-
+# Create new dataset from recipe
 MybakedData <- bake(rec_obj, new_data = Mydata) %>% select(-target)
 
+# split data into 4 equal size training sets from baked data
 Train_1 <- MybakedData[1:250000,]
 Train_2 <- MybakedData[250001:500000,]
 Train_3 <- MybakedData[500001:750000,]
 Train_4 <- MybakedData[750001:999999,]
 
+# Response Variables for each training set 
 y_1train <- Mydata[1:250000,1]
 y_2train <- Mydata[250001:500000,1]
 y_3train <- Mydata[500001:750000,1]
@@ -40,6 +42,7 @@ dim(Train_1)
 dim(y_1train)
 dim(Mydata[,1])
 
+# Makes sequential Network for Dataset 1
 model1 <- keras_model_sequential() %>%
   layer_dense(units = 256, activation = "relu", input_shape = ncol(Train_1)) %>%
   #layer_dropout(rate=0.4) %>% 
@@ -50,12 +53,14 @@ model1 <- keras_model_sequential() %>%
   #layer_dense(units = 128, activation = "relu") %>%
   layer_dense(units = 1, activation = "sigmoid")
 
+# Sets optimizer, loss, and metrics for model1
 model1 %>% compile(
   optimizer = "Adam",
   loss = "binary_crossentropy",
   metrics = c("accuracy")
 )
 
+# Runs (fit) the model and stores plottable info
 history1 <- model1 %>% fit(
   as.matrix(Train_1), y_1train,
   validation_split = .2,
@@ -63,6 +68,7 @@ history1 <- model1 %>% fit(
   batch_size = 256
 )
 
+model1
 plot(history1)
 
 model2 <- keras_model_sequential() %>%
